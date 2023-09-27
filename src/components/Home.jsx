87a2c26b552e89dcb53956b53d81af30e92f8b41
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-27 14:29:27 +0200

    added responsiveness fro homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import avatar from "../assets/avatar.png";
+import avatar from "../assets/rounded-avatar.png";
 import "../stylings/Avatar.css";
 import "../stylings/Home.css";
 import SocialLinks from "./SocialLinks";
@@ -14,12 +14,14 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1>Hi, I am <span className="emphasize-name">Chris!</span></h1>
+      <div className="avatar-text-wrapper">
       <img src={avatar} alt="my-avatar-picture" className="avatar" />
       <p>
         I am a <span className="emphasize-career">frontend web developer</span> passionate about crafting clean,
         pixel-perfect code to enhance user experiences through captivating web
         animations, creating engaging and interactive websites.
       </p>
+      </div>
       <SocialLinks />
     </div>
   );
