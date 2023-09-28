2b98c4541d49219c55ed6934bcd28db42d75e470
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-28 15:31:11 +0200

    styling for the about page added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from 'react';
 import { FcManager, FcMindMap, FcShop } from 'react-icons/fc';
 import '../stylings/About.css';
 import image2 from '../assets/about2.png';
-import image3 from '../assets/about3.png';
 import image4 from '../assets/about4.png';
 
-const images = [ image2, image3, image4];
+const images = [image2, image4];
 
 const About = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -24,9 +23,13 @@ const About = () => {
 
   return (
     <div className="about-container">
-      <h2 className="about-title">About me</h2>
-      <div className="about-item">
-        <img src={images[currentImageIndex]} alt="my-pictures" className="about-image" />
+      <div className="about-title">
+        <h2>About me</h2>
+      </div>
+      <div className="about-content">
+        <div className="about-gallery">
+          <img src={images[currentImageIndex]} alt="my-pictures" className="about-image" />
+        </div>
         <div className="description">
           <p className="motto">
             I am a relentless traveler with an unwaveringly positive outlook on life. My persistence knows no bounds as I take challenges with enthusiasm and determination.
